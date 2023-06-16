1261d43727cfd5ecdd7769c117838bd095d50960
Jorge Gonçalves <jorge.goncalves@bytepitch.com>
2023-06-16 12:06:52 +0100

    chore(CAW-300): linter action
    Co-authored-by: JanekGrosser <JanekGrosser@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
-    'prettier/prettier': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
   },
 };
