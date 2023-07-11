64acede0a9c9ee4e7ddf8d3172649434f3ed7998
Jan Grosser <jan.grosser@bytepitch.com>
2023-07-11 15:52:53 +0200

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,87 @@ const GetUnblock = (props: SdkProps): SDK => {
   );
 };
 
+// Types
+export * from './auth/definitions';
+
+export {
+  AddUserToCompanyRequest,
+  AddUserToCompanyResponse,
+  CompanyDetails,
+  CreateCompanyResponse,
+  RemoveUserFromCompanyRequest,
+  RemoveUserFromCompanyResponse,
+  TargetAddress,
+  UpdateCompanyResponse,
+} from './company/definitions';
+
+export { SdkProps } from './definitions';
+
+export * from './exchange-rates/definitions';
+
+export * from './kyc/definitions';
+
+export { GetUserOfframpAddressRequest, GetUserOfframpAddressResponse } from './offramp/definitions';
+
+export * from './process/definitions';
+
+export {
+  ChangeMainUserRemoteBankAccountRequest,
+  CreateRemoteUserBankAccount,
+  CreateRemoteUserBankAccountRequest,
+  CreateRemoteUserBankAccountResponse,
+  EurAccountDetails,
+  GbpAccountDetails,
+  GetAllRemoteBankAccountsRequest,
+  GetAllRemoteBankAccountsResponse,
+  GetRemoteBankAccountByUuidRequest,
+  GetRemoteBankAccountByUuidResponse,
+  RemoteUserBankAccount,
+} from './remote-bank-account/definitions';
+
+export {
+  GetUserTokenPreferenceResponse,
+  GetUserTokenPreferencesRequest,
+  TokenPreference,
+  TokenPreferenceArbitrum,
+  TokenPreferenceCelo,
+  TokenPreferenceMainnet,
+  TokenPreferenceOptimism,
+  TokenPreferencePolygon,
+  UpdateUserTokenPreferencesRequest,
+  UpdateUserTokenPreferencesResponse,
+} from './token-preference/definitions';
+
+export { TransactionFeeEstRequest, TransactionFeeEstResponse } from './transaction-fee/definitions';
+
+export {
+  CreateUnblockUserBankAccountRequest,
+  CreateUnblockUserBankAccountResponse,
+  GetAllunblockUserBankAccountsRequest,
+  GetAllunblockUserBankAccountsResponse,
+  GetUnblockBankAccountByIdRequest,
+  GetUnblockBankAccountByIdResponse,
+  SimulateOnRampRequest,
+  SimulateOnRampResponse,
+  UserBankAccount,
+  UserBankAccountDetails,
+  UserBankAccountFull,
+} from './unblock-bank-account/definitions';
+
+export * from './user/definitions';
+
+// Enums
+export * from './enums/AuthenticationMethod';
+export * from './enums/Chain';
+export * from './enums/CompanyType';
+export * from './enums/Country';
+export * from './enums/Currency';
+export * from './enums/PaymentMethods';
+export * from './enums/ProcessDirection';
+export * from './enums/ProcessStatus';
+export * from './enums/Tokens';
+export * from './enums/UserCompanyRole';
+export * from './enums/UserStatus';
+
+// Default
 export default GetUnblock;
