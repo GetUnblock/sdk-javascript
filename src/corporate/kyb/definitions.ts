9d29a34a340962fa78c235c1df0a28c0bfab5920
Przemek <32672299+Przemek-Win@users.noreply.github.com>
2023-09-04 17:59:08 +0200

    Added KYB endpoints

--- src/corporate/kyb/definitions.ts
+++ src/corporate/kyb/definitions.ts
@@ -1,7 +1,55 @@
 export type GetKybDocumentsChecklistRequest = {
-  not_defined_yet: true;
+  corporateUuid: string;
 };
 
 export type GetKybDocumentsChecklistResponse = {
-  not_defined_yet: true;
+  legalName: string,
+  tradingName: string,
+  dateOfFormation: string,
+  registeredAddress: RegisteredAddress,
+  tradingdAddress: TradingAddress,
+  hasParentCompany: boolean,
+  parentCompanyName: string,
+  parentCompanyIncDate: string,
+  parentCompanyRegisteredAddress: ParentCompanyRegisteredAddress,
+  parentCompanyActivities: string,
+  parentCompanySourceofIncome: string,
+  uboList: Ubo[],
+  pscList: Psc[],
+  sourceOfIncome: string
+
+};
+
+export type Psc = {
+  firstName: string,
+  lastName: string
+};
+
+export type Ubo = {
+  firstName: string,
+  lastName: string
+};
+
+export type ParentCompanyRegisteredAddress = {
+  addressLine1: string,
+  addressLine2: string,
+  postCode: string,
+  city: string,
+  country: string
+};
+
+export type TradingAddress = {
+  addressLine1: string,
+  addressLine2: string,
+  postCode: string,
+  city: string,
+  country: string
+};
+
+export type RegisteredAddress = {
+  addressLine1: string,
+  addressLine2: string,
+  postCode: string,
+  city: string,
+  country: string
 };
--- tests/corporate/KybService.spec.ts
+++ tests/corporate/KybService.spec.ts
@@ -1,4 +1,15 @@
 import axios, { AxiosError, AxiosInstance } from 'axios';
+import {
+  AddUserToCorporateApiRequestBody,
+  CorporateDetails,
+  CreateCorporateApiRequestBody,
+  CreateCorporateApiResponseData,
+  CreateCorporateResponse,
+  TargetAddress,
+  UpdateCorporateApiRequestBody,
+} from '../../src/corporate/management/definitions';
+import { KybService } from '../../src/corporate/kyb/KybService';
+
 import { SdkSettings } from '../../src/SdkSettings';
 import { axiosErrorMock, randomErrorMock } from '../mocks/errors.mock';
 import { propsMock } from '../mocks/props.mock';
@@ -14,6 +25,10 @@ describe('KybService', () => {
   let axiosError: AxiosError;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let randomError: unknown;
+  let corporateDetails: CorporateDetails;
+  let targetAddress: TargetAddress;
+  let message: string;
+  let uuid: string;
 
   beforeAll(() => {
     axiosClient = mockedAxios.create();
@@ -29,7 +44,62 @@ describe('KybService', () => {
     jest.resetAllMocks();
   });
 
-  describe.skip('getKybDocumentsChecklist', () => undefined);
+  describe.skip('getKybDocumentsChecklist', () => {
+
+    // Happy
+    it('should call axiosget method of KYB required', async () => {
+      // Arrange
+      const expectedResult: GetKybDocumentsChecklistResponse = {
+        message: message,
+        uuid: uuid,
+      };
+
+      const expectedPath = '/corporate';
+      const expectedBody: CreateCorporateApiRequestBody = {
+        name: corporateDetails.legal_name,
+        type: corporateDetails.type,
+        registered_address: corporateDetails.registeredAddress,
+        city: corporateDetails.city,
+        country: corporateDetails.country,
+        registration_number: corporateDetails.registrationNumber,
+        contact_name: corporateDetails.contactName,
+        phone: corporateDetails.phone,
+        email: corporateDetails.email,
+        industry_sector_type: corporateDetails.industrySectorType,
+        industry_sector_value: corporateDetails.industrySectorValue,
+        target_address: targetAddress.targetAddress,
+      };
+      const expectedConfig = {
+        headers: {
+          'content-type': 'application/json',
+          accept: 'application/json',
+          Authorization: props.apiKey,
+        },
+      };
+
+      const responseData: CreateCorporateApiResponseData = {
+        message: message,
+        uuid: uuid,
+      };
+
+      jest.spyOn(axios, 'create').mockReturnValueOnce(axiosClient);
+      jest.spyOn(axiosClient, 'post').mockResolvedValueOnce({
+        status: 200,
+        data: responseData,
+      });
+
+      const service = new KybService(props);
+
+      // Act
+      const result = await service.getChecklistOfDocuments({corporateUuid: uuid});
+
+      // Assert
+      expect(axiosClient.post).toBeCalledTimes(1);
+      expect(axiosClient.post).toHaveBeenLastCalledWith(expectedPath, expectedBody, expectedConfig);
+      expect(result).toStrictEqual(expectedResult);
+
+    });
+  });
 
   /**
    * @todo: Delete after implementing the required tests
@@ -38,3 +108,5 @@ describe('KybService', () => {
     expect(true).toBe(true);
   });
 });
+
+
